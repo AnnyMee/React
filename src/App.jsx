ee31c6b321a4f462e110a49e2fc9e76dba5f55f7
AnnyMee <anny.petruc@gmail.com>
2024-04-17 13:10:08 +0200

    yolo!

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Lesson02 from "./classwork/Lesson02/lesson02";
+
+function App() {
+  return (
+    <div className="App">
+        <Lesson02 />
+    </div>
+  );
+}
+
+export default App;
